e7cd56a6ee9411841a50525f1ec2d105b697ec60
jenniferho <jennifersho09@gmail.com>
2020-02-05 21:34:00 -0500

    second commit

--- app/src/main/java/com/hojennifer/quizapp/MainActivity.java
+++ app/src/main/java/com/hojennifer/quizapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.hojennifer.quizapp;
 
+
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,23 +15,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
-    EditText responseText;
+    EditText responseText1;
+    EditText responseText2;
     TextView displayView;
+    Button changeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         submitButton = findViewById(R.id.clickButton);
-        responseText = findViewById(R.id.responseEditText);
+        responseText1 = findViewById(R.id.responseEditText);
+        responseText2 = findViewById(R.id.responseEditText2);
         displayView = findViewById(R.id.textBox);
+        changeButton = findViewById(R.id.changeButton);
         submitButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Log.i("testButton", "Hello dad!" + responseText.getText().toString());
-                displayView.setText(displayView.getText().toString()+" "+responseText.getText().toString());
+                Log.i("userInfo", "" + responseText1.getText().toString() + ", https://github.com/" + responseText2.getText().toString());
+                displayView.setText("https://github.com/" + responseText2.getText().toString());
+                String gitLink = "https://github.com/" + responseText2.getText().toString();
+                String clickLink = " <a href=" + gitLink + ">Your GitHub link!</a>";
+                displayView.setMovementMethod(LinkMovementMethod.getInstance());
+                displayView.setText(Html.fromHtml(clickLink));
+
             }
         });
-        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        changeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                int color = ((ColorDrawable)changeButton.getBackground()).getColor();
+                Log.i("startColor", ""+color);
+                Log.i("origColor", ""+R.color.changeColorButton);
+                if(color == getResources().getColor(R.color.changeColorButton)){ //if orig, change to new
+                    changeButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                }
+                else{ //if new, change to orig
+                    changeButton.setBackgroundColor(getResources().getColor(R.color.changeColorButton));
+                }
+            }
+        });
+        /*responseText.setOnFocusChangeListener(new View.OnFocusChangeListener(){
             @Override
             public void onFocusChange(View v, boolean hasFocus){
                 if(!hasFocus) {
@@ -38,6 +66,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             }
-        });
+        });*/
     }
 }
